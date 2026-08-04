827c559f27fd8fd9e8dbe7adcbfd2aabfa28e507
FrankVollering <f.vollering@gmail.com>
2026-08-04 22:00:28 +0200

    changes

--- tiles.js
+++ tiles.js
@@ -11,15 +11,9 @@ const TILES = {
   cam1:          false,   // ferry cam (live video, iframe)
   roofwind:      true,   // Ecowitt roof wind meter
   windmap:       true,   // animated wind map
-<<<<<<< Updated upstream
-  cam2:          true,   // Kaag cam
-  pv:            false,   // solar production (dummy data) 
-  marinetraffic: false,  // AIS ship map
-=======
   cam2:          true,   // Kaag cam (live video, iframe)
   pv:            false,   // solar production (dummy data)
   marinetraffic: false,  // AIS ship map
   "cam1-snap":   true,  // ferry cam, snapshot version (lighter on the Pi than cam1)
   "cam2-snap":   false,  // Kaag cam, snapshot version (lighter on the Pi than cam2)
->>>>>>> Stashed changes
 };
